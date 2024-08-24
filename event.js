663890aed13870be785df1d8dff764e51d40aef1
Celine <161593235+scholasticaCeline@users.noreply.github.com>
2024-08-24 05:23:33 +0000

    Added carousel in events (unfinished)

--- event.js
+++ event.js
@@ -1,27 +1,143 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const carousel = document.querySelector('.expo-card-wrap');
-//     const prevButton = document.querySelector('.expo-button-left');
-//     const nextButton = document.querySelector('.expo-button-right');
+// PBP
+let places = [
+    {
+        place: "KEMANGGISAN",
+        details: [
+            {
+                date: "Senin, 32 Agustus 2077",
+                location: "Zoom",
+                meetingShift: "A",
+                time: "09.61 - 11.99",
+            },
+            {
+                date: "Selasa, 33 Agustus 2077",
+                location: "Zoom",
+                meetingShift: "B",
+                time: "10.00 - 12.00",
+            },
+            {
+                date: "Rabu, 34 Agustus 2077",
+                location: "Zoom",
+                meetingShift: "C",
+                time: "11.00 - 13.00",
+            },
+        ],
+    },
+    {
+        place: "SENAYAN",
+        details: [
+            {
+                date: "Kamis, 35 Agustus 2077",
+                location: "Campus",
+                meetingShift: "A",
+                time: "08.00 - 10.00",
+            },
+            {
+                date: "Jumat, 36 Agustus 2077",
+                location: "Campus",
+                meetingShift: "B",
+                time: "09.00 - 11.00",
+            },
+            {
+                date: "Sabtu, 37 Agustus 2077",
+                location: "Campus",
+                meetingShift: "C",
+                time: "10.00 - 12.00",
+            },
+        ],
+    },
+];
 
-//     let scrollAmount = 0;
-//     const cardWidth = document.querySelector('.expo-card').offsetWidth;
-//     const visibleCards = Math.floor(carousel.parentElement.offsetWidth / cardWidth);
+let currentPlaceIndex = 0;
+let currentSlideIndex = 0;
 
-//     nextButton.addEventListener('click', () => {
-//         if (scrollAmount < (carousel.scrollWidth - carousel.clientWidth)) {
-//             scrollAmount += cardWidth;
-//             carousel.style.transform = `translateX(-${scrollAmount}px)`;
-//         }
-//     });
+function updateCarousel() {
+    const place = places[currentPlaceIndex];
+    const details = place.details[currentSlideIndex];
 
-//     prevButton.addEventListener('click', () => {
-//         if (scrollAmount > 0) {
-//             scrollAmount -= cardWidth;
-//             carousel.style.transform = `translateX(-${scrollAmount}px)`;
-//         }
-//     });
-// });
+    document.getElementById("event-place").innerHTML = `
+        <h2>${place.place}</h2>
+    `;
 
+    document.getElementById("carousel-content").innerHTML = `
+    <p>${details.date}</p>
+    <p>Location : ${details.location}</p>
+    <p>MeetingShift : ${details.meetingShift}</p>
+    <p>Time : ${details.time}</p>
+  `;
+}
+
+function prevSlide() {
+    if (currentPlaceIndex == 0 && currentSlideIndex == 0) {
+        currentPlaceIndex = 1;
+        currentSlideIndex = places[currentPlaceIndex].details.length - 1;
+    } else if (currentSlideIndex == 0) {
+        currentSlideIndex = places[currentPlaceIndex].details.length - 1;
+        currentPlaceIndex--;
+    } else {
+        currentSlideIndex--;
+    }
+
+    const carouselContent = document.getElementById("carousel-content");
+    carouselContent.style.animation = "0.2s 1 pbpAnimationBackwards";
+    carouselContent.addEventListener(
+        "animationend",
+        function onEnd() {
+            carouselContent.style.removeProperty("animation");
+            carouselContent.style.animation =
+                "0.2s 1 pbpAnimationBackwardsAfter";
+            carouselContent.removeEventListener("animationend", onEnd);
+        },
+        { once: true },
+    );
+
+    // currentSlideIndex =
+    //     currentSlideIndex > 0
+    //         ? currentSlideIndex - 1
+    //         : places[currentPlaceIndex].details.length - 1;
+    updateCarousel();
+}
+
+function nextSlide() {
+    if (
+        currentPlaceIndex == 1 &&
+        currentSlideIndex == places[currentPlaceIndex].details.length - 1
+    ) {
+        currentPlaceIndex = 0;
+        currentSlideIndex = 0;
+    } else if (
+        currentSlideIndex ==
+        places[currentPlaceIndex].details.length - 1
+    ) {
+        currentSlideIndex = 0;
+        currentPlaceIndex++;
+    } else {
+        currentSlideIndex++;
+    }
+
+    // currentSlideIndex =
+    //     currentSlideIndex < places[currentPlaceIndex].details.length - 1
+    //         ? currentSlideIndex + 1
+    //         : 0;
+
+    const carouselContent = document.getElementById("carousel-content");
+    carouselContent.style.animation = "0.2s 1 pbpAnimation";
+    carouselContent.addEventListener(
+        "animationend",
+        function onEnd() {
+            carouselContent.style.removeProperty("animation");
+            carouselContent.style.animation = "0.2s 1 pbpAnimationAfter";
+            carouselContent.removeEventListener("animationend", onEnd);
+        },
+        { once: true },
+    );
+    updateCarousel();
+}
+
+// Initialize
+updateCarousel();
+
+// Expo
 let currCard = 2;
 document.getElementById("click-right").addEventListener("click", function () {
     currCard++;
